36f74e80f5b95444474445fc71a3a926cb51cc3b
inoue0406 <inoue0406+github@gmail.com>
2014-08-08 00:47:54 +0900

    Gave up using RandomFields

--- w-RandomFields/RF_test.R
+++ w-RandomFields/RF_test.R
@@ -4,6 +4,7 @@
 
 library(RandomFields)
 library(RColorBrewer)
+library(colorspace)
 
 # -------------------------------------
 # 1. Run example of RFsimulate
@@ -33,18 +34,12 @@ y.seq <- seq(from, to, length=len)
 
 # simulation
 simu <- RFsimulate(model, x=x.seq, y=y.seq, grid=TRUE)
-plot(simu)
+#plot(simu)
+# simple "plot" does not seem to work ..
+spplot(simu)
+# spplot works
 
 # Conditional simulation
-
-# first we simulate some random values at a
-# 100 random locations:
-n <- if (interactive()) 100 else 2
-x <- runif(n=n, min=-1, max=1)
-y <- runif(n=n, min=-1, max=1)
-data <- RFsimulate(model = RMexp(), x=x, y=y, grid=FALSE)
-plot(data)
-
 # let simulate a field conditional on the above data
 x.seq.cond <- y.seq.cond <- seq(-1.5, 1.5, length=n)
 model <- RMexp()
@@ -61,7 +56,7 @@ cond <- RFsimulate(model = model, x=x.seq.cond, y=y.seq.cond,
 RFoptions(seed=0)
 model <- RMmastein(RMgauss(), nu=1, delta=10)
 x <- seq(0, 10, if (interactive()) 0.1 else 3)
-plot(RFsimulate(model, x=x, y=x))
+spplot(RFsimulate(model, x=x, y=x))
 
 # Cox - Isham model
 RFoptions(seed=0)
@@ -76,5 +71,7 @@ plot(model, ylim=c(0, 1), MARGIN=c(1,2),fixed.MARGIN=0.2,dim=3)
 RFoptions(cPrintlevel=4) 
 # (x,y,t) simulation not working well?
 data <- RFsimulate(model, x=x, y=x,T=c(0,1,10))
-plot(data)
+spplot(data)
+
+# 
 
